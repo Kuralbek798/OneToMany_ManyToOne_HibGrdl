aad1ba67ec111527edf236269386a320c830b2d8
kuralbek <agubaev_kuralbek@mail.ru>
2022-05-15 23:43:04 +0600

    15052022

--- src/main/java/model/App.java
+++ src/main/java/model/App.java
@@ -4,6 +4,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -28,13 +30,22 @@ public class App {
 
            Persona persona = item.getOwner();
            System.out.println(persona);*/
-         Persona persona = session.get(Persona.class,2);
+        /* Persona persona = session.get(Persona.class,2);
          Item newItem = new Item ("Item from Hibernate",persona);
 
          persona.getItems().add(newItem);
 
          session.save(newItem);
 
+*/
+           Persona persona = new Persona("Test persona",30);
+
+          Item newItem = new Item("Item from Hibernate 2",persona);
+
+          persona.setItems(new ArrayList<>(Collections.singletonList(newItem)));
+
+          session.save (persona);
+          session.save(newItem);
 
             session.getTransaction().commit();
        }finally {
